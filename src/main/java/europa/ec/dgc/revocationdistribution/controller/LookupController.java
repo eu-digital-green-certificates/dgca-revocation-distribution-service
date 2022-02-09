b492275e44b990353ab1a29ca597496191c7199a
Simon Laurenz <simon.laurenz@t-systems.com>
2022-02-09 14:47:00 +0100

    Fixed endpoint path

--- src/main/java/europa/ec/dgc/revocationdistribution/controller/LookupController.java
+++ src/main/java/europa/ec/dgc/revocationdistribution/controller/LookupController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/lookup")
+@RequestMapping("/revocation/lookup")
 @Slf4j
 @RequiredArgsConstructor
 public class LookupController {
