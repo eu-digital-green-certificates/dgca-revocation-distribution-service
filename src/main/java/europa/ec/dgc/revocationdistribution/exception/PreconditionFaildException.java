786078e93050cc238363e9181a02f780b058f769
slaurenz <82034561+slaurenz@users.noreply.github.com>
2022-04-26 17:04:11 +0200

    Setup Openapi doc page for github

--- src/main/java/europa/ec/dgc/revocationdistribution/exception/PreconditionFaildException.java
+++
@@ -1,5 +0,0 @@
-package europa.ec.dgc.revocationdistribution.exception;
-
-public class PreconditionFaildException extends RuntimeException{
-
-}
--- src/main/java/europa/ec/dgc/revocationdistribution/repository/RevocationListJsonRepository.java
+++
@@ -1,8 +0,0 @@
-package europa.ec.dgc.revocationdistribution.repository;
-
-import europa.ec.dgc.revocationdistribution.entity.RevocationListJsonEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RevocationListJsonRepository extends JpaRepository<RevocationListJsonEntity, String> {
-
-}
